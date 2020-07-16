4e3fd47112daf67b2d6cfae8a149f1a370a2b240
Patrick Thomson <patrickt@github.com>
2020-07-16 15:41:30 -0400

    Ormolu.

--- src/Control/Carrier/Random/Lifted.hs
+++ src/Control/Carrier/Random/Lifted.hs
@@ -1,24 +1,35 @@
-{-# LANGUAGE AllowAmbiguousTypes, FlexibleContexts, FlexibleInstances, GADTs, GeneralizedNewtypeDeriving,
-             MultiParamTypeClasses, ScopedTypeVariables, TypeApplications, TypeOperators, UndecidableInstances #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 -- | This carrier lifts the internals of its random number generation into
 -- a 'LiftC' constraint, assuming the parameter to that 'LiftC' implements
 -- 'PrimMonad'. In practice, this means that your effect stack must terminate
 -- with @LiftC IO@ or @LiftC (ST s)@.
 module Control.Carrier.Random.Lifted
-  ( RandomC (..)
-  , runRandomSystem
-  , runRandomSeeded
-    -- * Random effect
-  , module Control.Effect.Random
-  ) where
+  ( RandomC (..),
+    runRandomSystem,
+    runRandomSeeded,
 
-import           Control.Algebra
-import           Control.Carrier.Lift
-import           Control.Carrier.Reader
-import           Control.Effect.Random
-import           Control.Effect.Sum
-import           Control.Monad.IO.Class
-import           Control.Monad.Primitive
+    -- * Random effect
+    module Control.Effect.Random,
+  )
+where
+
+import Control.Algebra
+import Control.Carrier.Lift
+import Control.Carrier.Reader
+import Control.Effect.Random
+import Control.Effect.Sum
+import Control.Monad.IO.Class
+import Control.Monad.Primitive
 import qualified System.Random.MWC as MWC
 import qualified System.Random.MWC.Distributions as Dist
 
@@ -26,32 +37,32 @@ newtype RandomC prim m a = RandomC (ReaderC (MWC.Gen (PrimState prim)) m a)
   deriving (Applicative, Functor, Monad, MonadFail, MonadIO)
 
 instance (Algebra sig m, Member (Lift n) sig, PrimMonad n) => Algebra (Random :+: sig) (RandomC n m) where
-  alg (L (Random dist k)) = do
+  alg hdl (L (Random dist)) ctx = do
     gen <- RandomC ask
     let act = case dist of
-          Uniform           -> MWC.uniform
-          UniformR r        -> MWC.uniformR r
-          Normal m d        -> Dist.normal m d
-          Standard          -> Dist.standard
-          Exponential s     -> Dist.exponential s
-          TruncatedExp s r  -> Dist.truncatedExp s r
-          Gamma s h         -> Dist.gamma s h
-          ChiSquare d       -> Dist.chiSquare d
-          Beta a b          -> Dist.beta a b
-          Categorical w     -> Dist.categorical w
+          Uniform -> MWC.uniform
+          UniformR r -> MWC.uniformR r
+          Normal m d -> Dist.normal m d
+          Standard -> Dist.standard
+          Exponential s -> Dist.exponential s
+          TruncatedExp s r -> Dist.truncatedExp s r
+          Gamma s h -> Dist.gamma s h
+          ChiSquare d -> Dist.chiSquare d
+          Beta a b -> Dist.beta a b
+          Categorical w -> Dist.categorical w
           LogCategorical lw -> Dist.logCategorical lw
-          Geometric0 p      -> Dist.geometric0 p
-          Geometric1 p      -> Dist.geometric1 p
-          Bernoulli p       -> Dist.bernoulli p
-          Dirichlet t       -> Dist.dirichlet t
-          Permutation n     -> Dist.uniformPermutation n
-          Shuffle v         -> Dist.uniformShuffle v
-
-    sendM @n (act gen) >>= k
-  alg (L (Save k)) = do
-    gen <- RandomC ask
-    sendM @n (MWC.save gen) >>= k
-  alg (R other) = RandomC (alg (R (handleCoercible other)))
+          Geometric0 p -> Dist.geometric0 p
+          Geometric1 p -> Dist.geometric1 p
+          Bernoulli p -> Dist.bernoulli p
+          Dirichlet t -> Dist.dirichlet t
+          Permutation n -> Dist.uniformPermutation n
+          Shuffle v -> Dist.uniformShuffle v
+    res <- sendM @n (act gen)
+    hdl _
+  -- alg hdl (L Save) ctx = do
+  --   gen <- RandomC ask
+  --   sendM @n (MWC.save gen) >>= k
+  -- alg (R other) = RandomC (alg (R (handleCoercible other)))
   {-# INLINE alg #-}
 
 -- | Run a computation, seeding its random values from the system random number generator.
@@ -64,7 +75,7 @@ runRandomSystem (RandomC act) = do
   runReader rand act
 
 -- | Run a computation, seeding its random values from an existing 'MWC.Seed'.
-runRandomSeeded :: forall m n sig a . (Has (Lift n) sig m, PrimMonad n) => MWC.Seed -> RandomC n m a -> m a
+runRandomSeeded :: forall m n sig a. (Has (Lift n) sig m, PrimMonad n) => MWC.Seed -> RandomC n m a -> m a
 runRandomSeeded s (RandomC act) = do
   rand <- sendM @n (MWC.restore s)
   runReader rand act
--- src/Control/Effect/Random.hs
+++ src/Control/Effect/Random.hs
@@ -1,78 +1,83 @@
-{-# LANGUAGE DeriveFunctor, ExistentialQuantification, FlexibleContexts, GADTs, RankNTypes, StandaloneDeriving #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
 -- | The @Random@ effect provides access to uniformly distributed random values of
 -- user-specified types or from well-known numerical distributions.
 --
 -- This is the “fancy” syntax that hides most details of randomness
 -- behind a nice API.
 module Control.Effect.Random
-  ( Random (..)
-  -- * Uniform distributions
-  , uniform
-  , uniformR
-  -- * Continuous distributions
-  , normal
-  , standard
-  , exponential
-  , truncatedExp
-  , gamma
-  , chiSquare
-  , beta
-  -- * Discrete distributions
-  , categorical
-  , logCategorical
-  , geometric0
-  , geometric1
-  , bernoulli
-  , dirichlet
-  -- * Permutations
-  , uniformPermutation
-  , uniformShuffle
-  -- * Introspection
-  , save
-  , Distrib (..)
-  -- * Re-exports
-  , MWC.Variate
-  , Has
-  ) where
+  ( Random (..),
 
-import           Control.Algebra
-import           Data.Vector.Generic (Vector)
+    -- * Uniform distributions
+    uniform,
+    uniformR,
+
+    -- * Continuous distributions
+    normal,
+    standard,
+    exponential,
+    truncatedExp,
+    gamma,
+    chiSquare,
+    beta,
+
+    -- * Discrete distributions
+    categorical,
+    logCategorical,
+    geometric0,
+    geometric1,
+    bernoulli,
+    dirichlet,
+
+    -- * Permutations
+    uniformPermutation,
+    uniformShuffle,
+
+    -- * Introspection
+    save,
+    Distrib (..),
+
+    -- * Re-exports
+    MWC.Variate,
+    Has,
+  )
+where
+
+import Control.Algebra
+import Data.Kind
+import Data.Vector.Generic (Vector)
 import qualified System.Random.MWC as MWC
 
 -- | GADT representing the functions provided by mwc-random.
 data Distrib a where
-  Uniform        :: MWC.Variate a => Distrib a
-  UniformR       :: MWC.Variate a => (a, a) -> Distrib a
-  Normal         :: Double -> Double -> Distrib Double
-  Standard       :: Distrib Double
-  Exponential    :: Double -> Distrib Double
-  TruncatedExp   :: Double -> (Double, Double) -> Distrib Double
-  Gamma          :: Double -> Double -> Distrib Double
-  ChiSquare      :: Int -> Distrib Double
-  Beta           :: Double -> Double -> Distrib Double
-  Categorical    :: Vector v Double => v Double -> Distrib Int
+  Uniform :: MWC.Variate a => Distrib a
+  UniformR :: MWC.Variate a => (a, a) -> Distrib a
+  Normal :: Double -> Double -> Distrib Double
+  Standard :: Distrib Double
+  Exponential :: Double -> Distrib Double
+  TruncatedExp :: Double -> (Double, Double) -> Distrib Double
+  Gamma :: Double -> Double -> Distrib Double
+  ChiSquare :: Int -> Distrib Double
+  Beta :: Double -> Double -> Distrib Double
+  Categorical :: Vector v Double => v Double -> Distrib Int
   LogCategorical :: Vector v Double => v Double -> Distrib Int
-  Geometric0     :: Double -> Distrib Int
-  Geometric1     :: Double -> Distrib Int
-  Bernoulli      :: Double -> Distrib Bool
-  Dirichlet      :: Traversable t => t Double -> Distrib (t Double)
-  Permutation    :: Vector v Int => Int -> Distrib (v Int)
-  Shuffle        :: Vector v a => v a -> Distrib (v a)
+  Geometric0 :: Double -> Distrib Int
+  Geometric1 :: Double -> Distrib Int
+  Bernoulli :: Double -> Distrib Bool
+  Dirichlet :: Traversable t => t Double -> Distrib (t Double)
+  Permutation :: Vector v Int => Int -> Distrib (v Int)
+  Shuffle :: Vector v a => v a -> Distrib (v a)
 
-data Random m k
-  = forall a . Random (Distrib a) (a -> m k)
-  | Save (MWC.Seed -> m k)
+data Random (m :: Type -> Type) k where
+  Random :: Distrib a -> Random m a
+  Save :: MWC.Seed -> Random m ()
 
 deriving instance Functor m => Functor (Random m)
-
-instance HFunctor Random where
-  hmap f (Random d k) = Random d (f . k)
-  hmap f (Save k)     = Save (f . k)
-
-instance Effect Random where
-  thread ctx handler (Random d k) = Random d (handler . (<$ ctx) . k)
-  thread ctx handler (Save  k)    = Save (handler . (<$ ctx) . k)
-
 
 -- | Generate a single uniformly distributed random variate.  The
 -- range of values produced varies by type:
@@ -104,10 +109,13 @@ uniformR r = send (Random (UniformR r) pure)
 {-# INLINE uniformR #-}
 
 -- | Generate a normally distributed random variate with given mean and standard deviation.
-normal :: Has Random sig m
-       => Double -- ^ Mean
-       -> Double -- ^ Standard deviation
-       -> m Double
+normal ::
+  Has Random sig m =>
+  -- | Mean
+  Double ->
+  -- | Standard deviation
+  Double ->
+  m Double
 normal m d = send (Random (Normal m d) pure)
 
 -- | Generate a normally distributed random variate with zero mean and unit variance.
@@ -115,83 +123,108 @@ standard :: Has Random sig m => m Double
 standard = send (Random Standard pure)
 
 -- | Generate an exponentially distributed random variate.
-exponential :: Has Random sig m
-            => Double -- ^ Scale parameter
-            -> m Double
+exponential ::
+  Has Random sig m =>
+  -- | Scale parameter
+  Double ->
+  m Double
 exponential s = send (Random (Exponential s) pure)
 
 -- | Generate truncated exponentially distributed random variate.
-truncatedExp :: Has Random sig m
-             => Double            -- ^ Scale parameter
-             -> (Double,Double)   -- ^ Range to which distribution is
-                                  --   truncated. Values may be negative.
-             -> m (Double)
+truncatedExp ::
+  Has Random sig m =>
+  -- | Scale parameter
+  Double ->
+  -- | Range to which distribution is
+  --   truncated. Values may be negative.
+  (Double, Double) ->
+  m (Double)
 truncatedExp s r = send (Random (TruncatedExp s r) pure)
 
 -- | Random variate generator for gamma distribution.
-gamma :: Has Random sig m
-      => Double                 -- ^ Shape parameter
-      -> Double                 -- ^ Scale parameter
-      -> m Double
+gamma ::
+  Has Random sig m =>
+  -- | Shape parameter
+  Double ->
+  -- | Scale parameter
+  Double ->
+  m Double
 gamma s d = send (Random (Gamma s d) pure)
 
 -- | Random variate generator for the chi square distribution.
-chiSquare :: Has Random sig m
-          => Int                -- ^ Number of degrees of freedom
-          -> m Double
+chiSquare ::
+  Has Random sig m =>
+  -- | Number of degrees of freedom
+  Int ->
+  m Double
 chiSquare d = send (Random (ChiSquare d) pure)
 
 -- | Random variate generator for the geometric distribution,
 -- computing the number of failures before success. Distribution's
 -- support is [0..].
-geometric0 :: Has Random sig m
-           => Double            -- ^ /p/ success probability lies in (0,1]
-           -> m Int
+geometric0 ::
+  Has Random sig m =>
+  -- | /p/ success probability lies in (0,1]
+  Double ->
+  m Int
 geometric0 p = send (Random (Geometric0 p) pure)
 
 -- | Random variate generator for geometric distribution for number of
 -- trials. Distribution's support is [1..] (i.e. just 'geometric0'
 -- shifted by 1).
-geometric1 :: Has Random sig m
-           => Double            -- ^ /p/ success probability lies in (0,1]
-           -> m Int
+geometric1 ::
+  Has Random sig m =>
+  -- | /p/ success probability lies in (0,1]
+  Double ->
+  m Int
 geometric1 p = send (Random (Geometric1 p) pure)
 
 -- | Random variate generator for Beta distribution
-beta :: Has Random sig m
-     => Double            -- ^ alpha (>0)
-     -> Double            -- ^ beta  (>0)
-     -> m Double
+beta ::
+  Has Random sig m =>
+  -- | alpha (>0)
+  Double ->
+  -- | beta  (>0)
+  Double ->
+  m Double
 beta a b = send (Random (Beta a b) pure)
 {-# INLINE beta #-}
 
 -- | Random variate generator for Dirichlet distribution
-dirichlet :: (Has Random sig m, Traversable t)
-          => t Double          -- ^ container of parameters
-          -> m (t Double)
+dirichlet ::
+  (Has Random sig m, Traversable t) =>
+  -- | container of parameters
+  t Double ->
+  m (t Double)
 {-# INLINE dirichlet #-}
 dirichlet t = send (Random (Dirichlet t) pure)
 
 -- | Random variate generator for Bernoulli distribution
-bernoulli :: Has Random sig m
-          => Double            -- ^ Probability of success (returning True)
-          -> m Bool
+bernoulli ::
+  Has Random sig m =>
+  -- | Probability of success (returning True)
+  Double ->
+  m Bool
 {-# INLINE bernoulli #-}
 bernoulli p = send (Random (Bernoulli p) pure)
 
 -- | Random variate generator for categorical distribution.
-categorical :: (Has Random sig m, Vector v Double)
-            => v Double          -- ^ List of weights [>0]
-            -> m Int
+categorical ::
+  (Has Random sig m, Vector v Double) =>
+  -- | List of weights [>0]
+  v Double ->
+  m Int
 {-# INLINE categorical #-}
 categorical v = send (Random (Categorical v) pure)
 
 -- | Random variate generator for categorical distribution where the
 --   weights are in the log domain. It's implemented in terms of
 --   'categorical'.
-logCategorical :: (Has Random sig m, Vector v Double)
-               => v Double          -- ^ List of logarithms of weights
-               -> m Int
+logCategorical ::
+  (Has Random sig m, Vector v Double) =>
+  -- | List of logarithms of weights
+  v Double ->
+  m Int
 logCategorical v = send (Random (LogCategorical v) pure)
 
 -- | Save the state of the random number generator to be used by subsequent
@@ -200,9 +233,10 @@ save :: Has Random sig m => m MWC.Seed
 save = send (Save pure)
 
 -- | Random variate generator for uniformly distributed permutations. It returns random permutation of vector [0 .. n-1]. This is the Fisher-Yates shuffle.
-uniformPermutation :: (Has Random sig m, Vector v Int)
-                   => Int
-                   -> m (v Int)
+uniformPermutation ::
+  (Has Random sig m, Vector v Int) =>
+  Int ->
+  m (v Int)
 uniformPermutation n = send (Random (Permutation n) pure)
 
 -- | Random variate generator for a uniformly distributed shuffle (all
@@ -211,7 +245,8 @@ uniformPermutation n = send (Random (Permutation n) pure)
 --
 -- Implementation details prevent a native implementation of the 'MWC.uniformShuffleM'
 -- function. Use the native API if this is required.
-uniformShuffle :: (Has Random sig m, Vector v a)
-               => v a
-               -> m (v a)
+uniformShuffle ::
+  (Has Random sig m, Vector v a) =>
+  v a ->
+  m (v a)
 uniformShuffle n = send (Random (Shuffle n) pure)
